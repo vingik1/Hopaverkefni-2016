ed9bb454c32ba24f551063225bc6d68bb6498897
Viktor Ingi Kárason <vingik@gmail.com>
2016-11-24 14:42:18 +0000

    GPS hnit data og lagfæring
    Hnitin bæt við í Airport Töfluna og lagað villur sem voru í IATACODE dálkunum í bæði Airport og FlightSchedule Töflunni

--- FreshAir/03_FreshAir_Operation_Data.sql
+++ FreshAir/03_FreshAir_Operation_Data.sql
@@ -44,52 +44,56 @@ insert into Cities(cityName,countryCode)values('New York','US');
 insert into Cities(cityName,countryCode)values('Athens','GR');
 
 -- Airports
-insert into Airports(IATAcode,airportName,CityID)values('KEF','Keflavik Airport',1);								-- Iceland
-insert into Airports(IATAcode,airportName,CityID)values('OSL','Gardermoen',2);										-- Norway
-insert into Airports(IATAcode,airportName,CityID)values('ARN','Arlanda',3);											-- Stockholm
-insert into Airports(IATAcode,airportName,CityID)values('CPH','Kastrup',4);											-- Danmark
-insert into Airports(IATAcode,airportName,CityID)values('HEL','Helsinki Vantaa',5);									-- Finland
-insert into Airports(IATAcode,airportName,CityID)values('LHR','London Heathrow',6);									-- Great Britain
-insert into Airports(IATAcode,airportName,CityID)values('STN','London Stansted',6);									-- Great Britain
-insert into Airports(IATAcode,airportName,CityID)values('CDG','Charles de Gaulle International',7);					-- France
-insert into Airports(IATAcode,airportName,CityID)values('FRA','Frankfurt am Main International',8);					-- Germany
-insert into Airports(IATAcode,airportName,CityID)values('HHN','Frankfurt-Hahn',8);									-- Germany
-insert into Airports(IATAcode,airportName,CityID)values('ZRH','Zürich Airport',9);									-- Swiss	
-insert into Airports(IATAcode,airportName,CityID)values('BCN','Barcelona International ',10);						-- Spain
-insert into Airports(IATAcode,airportName,CityID)values('SVO','Sheremetyevo International ',11);					-- Russia
-insert into Airports(IATAcode,airportName,CityID)values('WAW','Warsaw Chopin Airport',12);							-- Poland
-insert into Airports(IATAcode,airportName,CityID)values('FAE','Vagar Airport',13);									-- Faroe Islands
-insert into Airports(IATAcode,airportName,CityID)values('MXP','Malpensa International',14);							-- Italia
-insert into Airports(IATAcode,airportName,CityID)values('VIE','Vienna International',15);							-- Austria
-insert into Airports(IATAcode,airportName,CityID)values('IST','Istanbul Atatürk Airport',16);						-- Turkey
-insert into Airports(IATAcode,airportName,CityID)values('TLV','Ben Gurion International',17);						-- Israel
-insert into Airports(IATAcode,airportName,CityID)values('CAI','Cairo International',18);							-- Egypt
-insert into Airports(IATAcode,airportName,CityID)values('DOH','Doha International Airport',19);						-- Qatar
-insert into Airports(IATAcode,airportName,CityID)values('DAR','Julius Nyerere International Airport',20);			-- Tanzania
-insert into Airports(IATAcode,airportName,CityID)values('NBO','Jomo Kenyatta International',21);					-- Kenya
-insert into Airports(IATAcode,airportName,CityID)values('JNB','OR Tambo International',22);							-- South Africa						
-insert into Airports(IATAcode,airportName,CityID)values('THR','Mehrabad International Airport',23);					-- Iran
-insert into Airports(IATAcode,airportName,CityID)values('BGW','Baghdad International Airport',24);					-- Irak
-insert into Airports(IATAcode,airportName,CityID)values('BOM','Chhatrapati Shivaji International Airport',25);		-- India
-insert into Airports(IATAcode,airportName,CityID)values('PEK','Beijing Capital International',26);					-- China
-insert into Airports(IATAcode,airportName,CityID)values('HKG','Hong Kong International Airport',26);				-- China
-insert into Airports(IATAcode,airportName,CityID)values('BKK','Suvarnabhumi',27);									-- Thailand
-insert into Airports(IATAcode,airportName,CityID)values('HAN','Noi Bai International Airport',28);					-- Viet Nam
-insert into Airports(IATAcode,airportName,CityID)values('NRT','Narita International',29);							-- Japan
-insert into Airports(IATAcode,airportName,CityID)values('SYD','Sydney Kingsford Smith International',30);			-- Australia
-insert into Airports(IATAcode,airportName,CityID)values('AKL','Auckland International',31);							-- New Zealand
-insert into Airports(IATAcode,airportName,CityID)values('BOS','Logan International',32);							-- U.S.A.
-insert into Airports(IATAcode,airportName,CityID)values('LAX','Los Angeles International',33);						-- U.S.A.
-insert into Airports(IATAcode,airportName,CityID)values('YYZ','Lester B. Pearson International',34);				-- Canada
-insert into Airports(IATAcode,airportName,CityID)values('KIN','Norman Manley International',35);					-- Jamaica
-insert into Airports(IATAcode,airportName,CityID)values('GIG','Galeão - Antônio Carlos Jobim International',36);	-- Brasil
-insert into Airports(IATAcode,airportName,CityID)values('MEX','Licenciado Benito Juarez International Airport',37);	-- Mexico
-insert into Airports(IATAcode,airportName,CityID)values('GLA','Glasgow International Airport',38);					-- Great Britain
-insert into Airports(IATAcode,airportName,CityID)values('LIS','Lisbon Portela Airport',39);							-- Portugal
-insert into Airports(IATAcode,airportName,CityID)values('PRG','Prague Václav Havel Airport',40);					-- Check Republic
-insert into Airports(IATAcode,airportName,CityID)values('IZE','Ministro Pistarini International Airport',41);		-- Argentina
-insert into Airports(IATAcode,airportName,CityID)values('JFK','John F. Kennedy Airport',42);						-- U.S.A.
-insert into Airports(IATAcode,airportName,CityID)values('ATH','Athens International Airport',43);					-- Greece
+insert into Airports(IATAcode,airportName,Latitude,Longitude,CityID)
+values
+('AKL','Auckland International','-37.008055','174.791667',31),
+('ARN','Arlanda','59.651944','17.918611',3),  
+('ATH','Athens International Airport','37.936358','23.944467',43),
+('BCN','Barcelona International ','41.297078','2.078464',10),
+('BGW','Baghdad International Airport','33.262539','44.234577',24),
+('BKK','Suvarnabhumi','13.681108','100.747283',27),
+('BOM','Chhatrapati Shivaji International Airport','19.088686','72.867919',25), 
+('BOS','Logan International','42.362944','-71.006388',32),    
+('CAI','Cairo International','30.121944','31.405556',18),  
+('CDG','Charles de Gaulle International','49.009722','2.547778',7),
+('CPH','Kastrup','55.617917','12.655972',4), 
+('DAR','Julius Nyerere International Airport','-6.878110','39.202625',20),    
+('DOH','Doha International Airport','25.273056','51.608056',19),   
+('FAE','Vagar Airport','62.063628','-7.277218',13),
+('FRA','Frankfurt am Main International','50.033306','8.570456',8),   
+('GIG','Galeão - Antônio Carlos Jobim International','-22.809999','-43.250555',36), 
+('GLA','Glasgow International Airport','55.871944','-4.433055',38),   
+('HAN','Noi Bai International Airport','21.221192','105.807178',28),    
+('HEL','Helsinki Vantaa','60.317222','24.963333',5),  
+('HHN','Frankfurt-Hahn','49.948672','7.263892',8),
+('HKG','Hong Kong International Airport','22.308889','113.914722',26),  
+('IST','Istanbul Atatürk Airport','40.976111','28.813889',16),    
+-- Breyt IATA code IZE er vitlaust EZE er rétta	https://en.wikipedia.org/wiki/Ministro_Pistarini_International_Airport
+('EZE','Ministro Pistarini International Airport','-34.822221','-58.535832',41),
+('JFK','John F. Kennedy Airport','40.639926','-73.778694',42),    
+('JNB','OR Tambo International','-26.133693','28.242317',22),
+('KEF','Keflavik Airport','63.985000','-22.605555',1),
+('KIN','Norman Manley International','17.935667','-76.787499',35),   
+('LAX','Los Angeles International','33.942625','-118.407802',33),    
+('LHR','London Heathrow','51.477500','-0.461388',6),    
+('LIS','Lisbon Portela Airport','38.781311','-9.135918',39),  
+('MEX','Licenciado Benito Juarez International Airport','19.436303','-99.072096',37),   
+('MXP','Malpensa International','45.630606','8.728111',14),    
+('NBO','Jomo Kenyatta International','-1.319241','36.927775',21),  
+('NRT','Narita International','35.765278','140.385556',29),   
+('OSL','Gardermoen','60.193917','11.100361',2),   
+('PEK','Beijing Capital International','40.073','116.598',26), 
+('PRG','Prague Václav Havel Airport','50.100833','14.260000',40),
+('STN','London Stansted','51.885000','0.235000',6),    
+('SVO','Sheremetyevo International','(55.972500','37.413056',11),    
+('SYD','Sydney Kingsford Smith International','-33.946110','151.177222',30),   
+('THR','Mehrabad International Airport','35.689167','51.313416',23),
+('TLV','Ben Gurion International','32.011389','34.886667',17),
+('VIE','Vienna International','48.110278','16.569722',15),
+('WAW','Warsaw Chopin Airport','52.165750','20.967122',12),  
+('YYZ','Lester B. Pearson International','43.676667','-79.630555',34),   
+('ZRH','Zürich Airport','47.464722','8.549167',9);
+					-- Greece
 
 -- Price Categories
 insert into PriceCategories(categoryName,validFrom,validTo,minimumPrice,refundable,seatNumberRestrictions,classID)
@@ -209,8 +213,8 @@ insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,d
 insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA302','MEX','KEF',7460);
 insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA305','KEF','GIG',9867);
 insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA306','GIG','KEF',9867);
-insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA303','KEF','IZE',11405);
-insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA304','IZE','KEF',11405);
+insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA303','KEF','EZE',11405);
+insert into FlightSchedules(flightNumber,originatingAirport,destinationAirport,distance)values('FA304','EZE','KEF',11405);
 
 -- ScheduleWeekdays  Keflavik - Oslo - Keflavik
 insert into ScheduleWeekdays(weekday,departure,flightNumber)values(1,'08:10','FA501');
